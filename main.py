5c74f963bed43c1e48ffad05a7a6c9eef0182ea0
Sherzod <daminovsherzod2002@gmail.com>
2022-07-14 12:00:50 +0500

    main.py

--- main.py
+++ main.py
@@ -21,8 +21,11 @@ def total_expenses(monthly_expenses: dict) -> int:
     Returns:
         total_expenses: total expenses
     """
-    return 
-
+    total = 0
+    for k in monthly_expenses.keys():
+        total += monthly_expenses[k]
+    return total
+print(total_expenses(monthly_expenses))
 # Find the least expensive expense
 def least_expensive(monthly_expenses: dict) -> str:
     """
@@ -32,8 +35,14 @@ def least_expensive(monthly_expenses: dict) -> str:
     Returns:
         least_expensive: least expensive expense
     """
-    return
-
+    min_v = list(monthly_expenses.values())[0]
+    ans = list(monthly_expenses.keys())[0]
+    for k,v in monthly_expenses.items():
+        if min_v>v:
+            min_v = v
+            ans = k
+    return ans
+print(least_expensive(monthly_expenses))
 # Find the most expensive expense
 def most_expensive(monthly_expenses: dict) -> str:
     """
@@ -43,4 +52,11 @@ def most_expensive(monthly_expenses: dict) -> str:
     Returns:
         most_expensive: most expensive expense
     """
-    return
+    mac_v = list(monthly_expenses.values())[0]
+    ans = list(monthly_expenses.keys())[0]
+    for k,v in monthly_expenses.items():
+        if mac_v<v:
+            mac_v = v
+            ans = k
+    return ans 
+print(most_expensive(monthly_expenses))    
